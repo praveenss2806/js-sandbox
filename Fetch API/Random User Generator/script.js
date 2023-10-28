7e0fe32ba9a4062611dcc8fd787ff976b20e84ed
Praveen S <praveenss2806@gmail.com>
2023-10-28 13:35:36 +0530

    code for randome user proj

--- Fetch API/Random User Generator/script.js
+++ Fetch API/Random User Generator/script.js
@@ -1,20 +1,52 @@
 const fetchUser = () => {
-    fetch('https://randomuser.me/api')
+    fetch('https://www.randomuser.me/api')
         .then((res) => {
             return res.json();
         })
         .then((data) => {
-            console.log(data.results[0]);
-            displayUser(data.results[0]);
+            putUserData(data.results[0]);
         });
 };
 
-const displayUser = (user) => {
+const putUserData = (user) => {
     if (user.gender === 'male') {
-        document.body.style.backgroundColor = 'steelblue';
+        document.body.style.backgroundColor = '#4682b4';
     } else {
         document.body.style.backgroundColor = 'purple';
     }
+
+    const userContent = document.querySelector('#user');
+
+    userContent.innerHTML = `
+      <div id="user" class="mt-6">
+         <div class="flex justify-between">
+          <div class="flex">
+            <img
+              class="w-48 h-48 rounded-full mr-8"
+              src="${user.picture.large}"
+            />
+            <div class="space-y-3">
+              <p class="text-xl">
+                <span class="font-bold">Name: </span>${user.name.title} ${user.name.first} ${user.name.last}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Email: </span> ${user.email}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Phone: </span> ${user.phone}
+              </p>
+              <p class="text-xl">
+                <span class="font-bold">Location: </span> ${user.location.city}
+              </p>
+              <p class="text-xl"><span class="font-bold">Age: </span> ${user.registered.age}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
 };
 
 fetchUser();
+const generateUser = document
+    .querySelector('button')
+    .addEventListener('click', fetchUser);
